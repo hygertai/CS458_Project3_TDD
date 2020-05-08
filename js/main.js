5da58ff23e8fca1355c1f8ff563324610e49c062
Gerta <hygerta.imeri@hotmail.com>
2020-05-08 21:03:30 +0300

    get user input Coordinates

--- js/main.js
+++ js/main.js
@@ -58,18 +58,28 @@
       mymap.locate({setView: true, maxZoom: 15});
     });
 
-    function validate (input) {
-        if($(input).attr('type') == 'number' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
+    $('.input100').on('click', function(){
+      var lat = document.getElementsByName("lat")[0].value;
+      var long = document.getElementsByName("long")[0].value;
+
+      L.marker([lat, long]).addTo(mymap)
+				.bindPopup("<b>Hello!</b><br />This is your location.").openPopup();
+      // mymap.locate({setView: true, maxZoom: 15});
+    });
+
+
+    // function validate (input) {
+    //     if($(input).attr('type') == 'number' || $(input).attr('name') == 'email') {
+    //         if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+    //             return false;
+    //         }
+    //     }
+    //     else {
+    //         if($(input).val().trim() == ''){
+    //             return false;
+    //         }
+    //     }
+    // }
 
     function showValidate(input) {
         var thisAlert = $(input).parent();

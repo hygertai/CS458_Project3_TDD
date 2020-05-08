6096fcc4ca0142ad4da245d5d74392585ec2c946
Gerta <hygerta.imeri@hotmail.com>
2020-05-08 19:58:57 +0300

    get location using GPS done

--- js/main.js
+++ js/main.js
@@ -42,7 +42,6 @@
                 check=false;
             }
         }
-
         return check;
     });
 
@@ -54,8 +53,13 @@
         });
     });
 
+    //$('.validate-form .contact100-form-btn').click(function() { locateControl.locate(); });
+    $('.pure-button').on('click', function(){
+      mymap.locate({setView: true, maxZoom: 15});
+    });
+
     function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+        if($(input).attr('type') == 'number' || $(input).attr('name') == 'email') {
             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
             }
@@ -79,17 +83,41 @@
         $(thisAlert).removeClass('alert-validate');
     }
 
-
-    function distanceToCityCenter() {
-      var lat1 = document.getElementById("lat").value;
-      var long1 = document.getElementById("long").value;
-      var lat2 = document.getElementById("lat").value;
-      var long2 = document.getElementById("lat").value;
-      const fi1 = lat1 * Math.PI/180, fi2 = lat2 * Math.PI/180, delta_lambda = (long2-long1) * Math.PI/180, R = 6371e3;
-      const d = Math.acos( Math.sin(fi1)*Math.sin(fi2) + Math.cos(fi1)*Math.cos(fi2) * Math.cos(delta_lambda) ) * R;
-
-      return d;
-    }
-
-
 })(jQuery);
+
+  //check for location, if found show marker
+  mymap.on('locationfound', onLocationFound);
+  function onLocationFound(e) {
+    console.log(e);
+    L.marker(e.latlng).addTo(mymap);
+  }
+
+function distanceToCityCenter() {
+  var lat1 = document.getElementById("lat").value;
+  var long1 = document.getElementById("long").value;
+  var lat2 = document.getElementById("lat").value;
+  var long2 = document.getElementById("lat").value;
+  const fi1 = lat1 * Math.PI/180, fi2 = lat2 * Math.PI/180, delta_lambda = (long2-long1) * Math.PI/180, R = 6371e3;
+  const d = Math.acos( Math.sin(fi1)*Math.sin(fi2) + Math.cos(fi1)*Math.cos(fi2) * Math.cos(delta_lambda) ) * R;
+
+  return d;
+}
+
+
+// navigator.geolocation.getCurrentPosition(function(location) {
+//     var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+//
+//     var mymap = L.map('mapid').setView(latlng, 13)
+//     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={access_token}', {
+//       maxZoom: 18,
+//       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+//         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+//         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//       id: 'mapbox/streets-v11',
+//       tileSize: 512,
+//       zoomOffset: -1,
+//       access_token: 'pk.eyJ1IjoiaGkxNCIsImEiOiJjazl4bTNrbmIwMmZ0M251c2psbTBkazU2In0.Xb2Rpdxp7u-XPRinPyf_tw'
+//     }).addTo(mymap);
+//
+//     var marker = L.marker(latlng).addTo(mymap);
+// });
